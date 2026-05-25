51f5b415886ee19a039d7c4aad91f66577234255
Your Name <you@example.com>
2026-05-25 15:11:05 +0300

    Еще одно изменение

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def hell(name):
-    print(f'Hello world and {name}')
+def hell(name1,name2):
+    print(f'Hello world and {name1}, {name2}')
 if __name__ == '__main__':
-    hell('Mr Z')
+    hell('Mr Z','Mrs Z')
